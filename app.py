2b6a730e77699d8c38c51d8b4cb994b54714bffd
Marcos Alexander <marcosavila3005@gmail.com>
2024-02-13 23:59:27 -0400

    Validaciones de campos vacios en Backend.

--- app.py
+++ app.py
@@ -15,15 +15,20 @@ def create_app():
     @app.route("/", methods = ["GET", "POST"])
     def home():
         if request.method == "POST":
-            entry_content = request.form.get("content")
-            fotmatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
 
+            entry_content = request.form.get("content")
             name_user = request.form.get("name")
+
+            if not entry_content or not name_user:
+                Error = "Unos de los dos formularios tiene esta vacio."
+                return render_template("home.html", error=Error)
+
+            fotmatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
 
             app.db.entries.insert_one( {
                 "content": entry_content,
                 "date": fotmatted_date,
-                "name": name_user,
+                "name": name_user
                 } )
             
         entries_with_date = [ 
@@ -35,10 +40,9 @@ def create_app():
             )
             
             for entry in app.db.entries.find({})
-            ]
-            
+            ]            
 
 
-        return render_template("home.html", entries=entries_with_date )
+        return render_template("home.html", entries=entries_with_date)
     
     return app
